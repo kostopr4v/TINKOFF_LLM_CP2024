445ef188537f8f924875b5e96892fa17687bc1cb
kostopr4v <68643585+kostopr4v@users.noreply.github.com>
2024-06-02 00:49:35 +0300

    Add files via upload

--- бот/tts_model.py
+++ бот/tts_model.py
@@ -4,6 +4,8 @@ import torch
 tts_model = TTS("tts_models/multilingual/multi-dataset/xtts_v2", gpu=True)
 
 def text2speech(text, file_path):
+    text = text.split('Подробнее: \nhttps://')[0]
+    print(text)
     tts_model.tts_to_file(text=text,
                 file_path=file_path,
                 speaker_wav="путин.mp3",
